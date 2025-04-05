d47ee30c283e1d3273a8e627d9d81b51142a2897
strad-dev131 <stradiogamer@gmail.com>
2025-04-05 17:07:09 +0530

    1 config.py

--- config.py
+++ config.py
@@ -41,8 +41,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/TheToxicBots")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/TheToxicDevelopers")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/TeamXUpdate")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/TeamsXchat")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
