8a90770d750fe7502f9b7044386485c547705d68
strad-dev131 <stradiogamer@gmail.com>
2025-04-14 22:53:51 +0530

    start.py

--- TeamX/utils/inline/start.py
+++ TeamX/utils/inline/start.py
@@ -26,16 +26,13 @@ def private_panel(_):
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-          # InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+         InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+         InlineKeyboardButton("[❄️] 𝐁σт 𝐈иғσ [❄️]", callback_data="bot_info_data"),
         ],
         # [
         #     InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
         #     InlineKeyboardButton(text=_["S_B_7"], user_id=config.CO_OWNER_ID),
         # ],
-        [
-            InlineKeyboardButton("[❄️] 𝐁σт 𝐈иғσ [❄️]", callback_data="bot_info_data"),
-        ],
     ]
     return buttons
 
